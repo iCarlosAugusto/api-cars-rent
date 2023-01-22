af5ebf7aad1355496bc3240bb7610ec1083b0b55
Carlos Augusto <carlosaugusto@bodyfitteam.com.br>
2023-01-22 18:45:45 -0300

    Criptografando as senhsa

--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { UserRepository } from "../../repositories/implementations/UserRepository";
+import { hash } from "bcrypt";
 
 @injectable()
 class CreateUserUseCase {
@@ -10,10 +11,13 @@ class CreateUserUseCase {
     ) {};
 
     async execute({name, email, password, driver_license}: ICreateUserDTO){
+
+        const passwordHash = await hash(password, 8);
+
         await this.userRepository.create({
             name,
             email,
-            password,
+            password: passwordHash,
             driver_license
         });
     };
